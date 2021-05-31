7234aafebd9ddca96081c5139a25f1e3b4e2f5b0
rd-goncalves <raphaelnoda@hotmail.com>
2021-05-31 19:04:11 -0300

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 function App() {
-  console.log(generatePalette(seedColors[4]));
 
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>Palette List goes here</h1>} />
+      <Route exact path='/palette/:id' render={() => <h1>Specific palette</h1>} />
+    </Switch>
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
